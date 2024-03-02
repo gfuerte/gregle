dbcdafe301f35eff67294c726cf6af764fb5081a
gfuerte <gregorio.fuerte@gmail.com>
2024-03-02 04:19:58 -0500

    Install MUI, configure prettier, add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react'
+import { NavigationBar } from './widgets/navigation/NavigationBar'
 
 function App() {
     return (
         <Fragment>
+            <NavigationBar />
             <h1>Gregle</h1>
         </Fragment>
     )
---
+++ src/widgets/navigation/NavigationBar.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NavigationBar = () => {
+    return <h1>Test</h1>
+}
