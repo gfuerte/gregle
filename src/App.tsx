8bdd1bdea8920743920c7899fca72ad396c5248d
gfuerte <gregorio.fuerte@gmail.com>
2024-03-02 04:08:01 -0500

    Install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from 'react'
 
 function App() {
-  return (
-    <Fragment>
-      <h1>Gregle</h1>
-      <h1>Gregle</h1>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <h1>Gregle</h1>
+        </Fragment>
+    )
 }
 
-export default App;
+export default App
